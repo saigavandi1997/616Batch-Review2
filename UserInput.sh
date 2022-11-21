5aab68b71c67597f5c1ac0bdc099c2dc6fd6aaba
Saigavandi1997 <saigavandi1997@gmail.com>
2022-11-21 08:45:29 +0530

    [saikanchan]-Array Index

--- UserInput.sh
+++ UserInput.sh
@@ -1,12 +1,5 @@
 #!/bin/bash -x
-echo "Enter Num1"
-read num1
-echo "Enter Num2"
-read num2
 
-if [ $num1 -gt $num2  ]
-then
-    echo $num1
-else
-    echo $num2 
-fi
+index_array=(1 2 3 4 5 6)
+echo ${index_array[2]}
+echo ${index_array[*]}
